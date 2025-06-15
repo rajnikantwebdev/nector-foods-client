5e36092a4ca7dcd31a0acfd84a8cd241b9cefcd6
rajnikantwebdev <rajnikantdevep@gmail.com>
2025-06-15 22:23:22 +0530

    updated routes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from "react-router";
-import RegisterForm from './components/RegisterForm.jsx';
-import LoginForm from './components/LoginForm.jsx';
-import Navbar from './components/Navbar.jsx';
-import ProtectedRoute from './components/ProtectedRoutes.jsx';
-import PublicRoute from './components/PublicRoute.jsx';
-import currentRoute from '../utils/currentRoute.js';
+import { BrowserRouter } from "react-router";
 import Layout from './Layout.jsx';
 createRoot(document.getElementById("root")).render(
   <StrictMode>
